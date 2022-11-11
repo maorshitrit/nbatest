132f7146a959a1871458f99a83151d86a26cde30
kesher <kesher@DESKTOP-K9V674O>
2022-11-11 10:02:09 +0200

    notification added

--- nba/src/nba/main.java
+++ nba/src/nba/main.java
@@ -1,5 +1,11 @@
 package nba;
 
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,6 +15,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.management.Notification;
 import javax.security.auth.login.CredentialNotFoundException;
 
 import org.json.JSONArray;
@@ -22,14 +29,31 @@ public class main {
 	static final String API_KEY = "a5fb04339f84532afc45e0c034c02590";
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException {
 		// TODO Auto-generated method stub
 
 		System.out.println("test");
 		System.out.print("david");
 		System.out.println("tetbsro");	
 		Getlivescores();
-		
+		createnotification();
+	}
+	public static void createnotification() throws AWTException {
+		 SystemTray tray = SystemTray.getSystemTray();
+
+	        //If the icon is a file
+	        Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
+	        //Alternative (if the icon is on the classpath):
+	        //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
+
+	        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
+	        //Let the system resize the image if needed
+	        trayIcon.setImageAutoSize(true);
+	        //Set tooltip text for the tray icon
+	        trayIcon.setToolTip("System tray icon demo");
+	        tray.add(trayIcon);
+
+	        trayIcon.displayMessage("Hello, World", "notification demo", MessageType.INFO);
 	}
 	
 	public static void Getlivescores()
